c65e6411faac69979d95f79a7c0ed0363d547307
Christelle Quilang <christelle.quilang@yahoo.co.nz>
2022-06-17 08:17:45 +1200

    Create App skeleton

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,20 +1,28 @@
 import React from 'react'
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 import Nav from './Nav'
+import Calendar from './Calendar'
+import Detail from './Detail'
+import Add from './Add'
+import Edit from './Edit'
 
 function App() {
   return (
     <>
-    <div className="title">
-      <h1>A+ Budget Calendar</h1>
-    </div>
-    {/* This 'main' div is only for styling (so we can use flexbox) */}
-    <div className="main">
-      <Nav />
-      <Routes>
-      </Routes>
-    </div>
+      <div className="title">
+        <h1>A+ Budget Calendar</h1>
+      </div>
+      {/* This 'main' div is only for styling (so we can use flexbox) */}
+      <div className="main">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Calendar />} />
+          <Route path="/detail/:id" element={<Detail />} />
+          <Route path="/detail/:id/add" element={<Add />} />
+          <Route path="/detail/:id/edit" element={<Edit />} />
+        </Routes>
+      </div>
     </>
   )
 }
