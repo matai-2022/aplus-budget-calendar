34db67fbc10813a1218492bf9fdcc4de6eccc64f
yiyuchen-nz <yiyuchen9@gmail.com>
2022-06-16 22:04:09 +1200

    added mock data

---
+++ data/calendar.js
@@ -0,0 +1,66 @@
+export default [
+  {
+    id: 1,
+    dayOfWeek: 'Monday',
+    transactions: [
+      { transactionId: 1, description: 'Coffee', amount: 5 },
+      { transactionId: 2, description: 'Food', amount: 19.5 },
+      { transactionId: 3, description: 'Gym', amount: 21 },
+    ],
+  },
+  {
+    id: 2,
+    dayOfWeek: 'Tuesday',
+    transactions: [
+      { transactionId: 1, description: 'Coffee', amount: 5 },
+      { transactionId: 2, description: 'Rent', amount: 200 },
+      { transactionId: 3, description: 'Utilities', amount: 65 },
+    ],
+  },
+  {
+    id: 3,
+    dayOfWeek: 'Wednesday',
+    transactions: [
+      { transactionId: 1, description: 'Food', amount: 15 },
+      { transactionId: 2, description: 'Coffee', amount: 5 },
+      { transactionId: 3, description: 'Phone', amount: 29 },
+      { transactionId: 4, description: 'Birthday gift for Mum', amount: 195 },
+      { transactionId: 5, description: 'Student Loan', amount: 300 },
+    ],
+  },
+  {
+    id: 4,
+    dayOfWeek: 'Thursday',
+    transactions: [
+      { transactionId: 1, description: 'Coffee', amount: 5 },
+      { transactionId: 2, description: 'Food', amount: 12.5 },
+    ],
+  },
+  {
+    id: 5,
+    dayOfWeek: 'Friday',
+    transactions: [
+      { transactionId: 1, description: 'Coffee', amount: 6.5 },
+      { transactionId: 2, description: 'Insurance', amount: 115 },
+    ],
+  },
+  {
+    id: 6,
+    dayOfWeek: 'Saturday',
+    transactions: [
+      { transactionId: 1, description: 'Clothing', amount: 98 },
+      { transactionId: 2, description: 'Hair', amount: 45 },
+      { transactionId: 3, description: 'Bubble Tea', amount: 7.5 },
+      { transactionId: 4, description: 'Restaurant', amount: 80 },
+    ],
+  },
+  {
+    id: 7,
+    dayOfWeek: 'Sunday',
+    transactions: [
+      { transactionId: 1, description: 'Groceries', amount: 180 },
+      { transactionId: 2, description: 'Dog Grooming', amount: 100 },
+      { transactionId: 3, description: 'Gas', amount: 80 },
+    ],
+  },
+]
