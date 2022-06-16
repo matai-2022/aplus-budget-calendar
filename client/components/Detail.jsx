c543dc813eed821f33289a8f2c0ee0acdbf85200
Christelle Quilang <christelle.quilang@yahoo.co.nz>
2022-06-17 11:04:02 +1200

    Create dynamic Detail page, inc Edit link & balance

--- client/components/Detail.jsx
+++ client/components/Detail.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import { Link, useParams } from 'react-router-dom'
 
-function Detail() {
+import budget from '../../data/budget'
+
+function Detail({ balance, transactions, total }) {
+  const { id } = useParams()
+  const dayOfWeek = budget.find((dayObject) => dayObject.id === id).dayOfWeek
   return (
-    <div>Detail</div>
+    <>
+      <div className="detail-day-name">{dayOfWeek}</div>
+      <div className="balance">{balance}</div>
+      <table className="detail-item-table">
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactions.map((transaction) => {
+            return (
+              <>
+                <tr>
+                  <td>{transaction.description}</td>
+                  <td>{transaction.amount}</td>
+                </tr>
+              </>
+            )
+          })}
+        </tbody>
+        <tfoot>
+          <td>Total</td>
+          <td>{total}</td>
+        </tfoot>
+      </table>
+      <Link to="/detail/:id/edit">Edit</Link>
+    </>
   )
 }
 
