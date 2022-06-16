13b8adeccc89fd7a413509453ac8f1ff3f1b7358
Christelle Quilang <christelle.quilang@yahoo.co.nz>
2022-06-17 10:04:57 +1200

    Create App skeleton

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -14,14 +14,11 @@ function App() {
       <div className="contents">
         <Routes>
           <Route path="/" element={<Calendar />} />
+          <Route path="/detail/:id" element={<Detail />} />
+          <Route path="/detail/:id/add" element={<Add />} />
+          <Route path="/detail/:id/edit" element={<Edit />} />
         </Routes>
       </div>
-      <Routes>
-        <Route path="/" element={<Calendar />} />
-        <Route path="/detail/:id" element={<Detail />} />
-        <Route path="/detail/:id/add" element={<Add />} />
-        <Route path="/detail/:id/edit" element={<Edit />} />
-      </Routes>
       <div className="footer">&copy; Team Ponyo 2022</div>
     </>
   )
