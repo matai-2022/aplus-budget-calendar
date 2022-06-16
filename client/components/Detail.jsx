425b72e33759e451df425bac1bf8ce70603541ff
Phuong Tran <ptranny@gmail.com>
2022-06-17 10:24:25 +1200

    Add JSX to Detail component

--- client/components/Detail.jsx
+++ client/components/Detail.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
 
-function Detail() {
+function Detail({transactions}) {
   return (
-    <div>Detail</div>
+    <>
+    <div className="detail-day-name">Monday</div>
+    <table className="detail-item-table">
+      <thead>
+        <tr>
+          <th>Description</th>
+          <th>Amount</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Coffee</td>
+          <td>-$4.50</td>
+        </tr>
+        <tr>
+          <td>Wages</td>
+          <td>$1000</td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <td>Total</td>
+        <td>$950.50</td>
+      </tfoot>
+    </table>
+    </>
   )
 }
 
