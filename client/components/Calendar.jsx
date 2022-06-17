76f4dfbafa1f168a495c26c967e57acf5d76056f
ysmael-silverio <silverioysmael@gmail.com>
2022-06-17 13:13:10 +1200

    negative in red

--- client/components/Calendar.jsx
+++ client/components/Calendar.jsx
@@ -21,6 +21,8 @@ function Calendar() {
       budget[i].total = sumDaily(budget[i].transactions)
       budget[i].balance = budget[i-1].balance + budget[i].total
   }
+
+
 }
   calcBalance(initialBalance, budget)
 
